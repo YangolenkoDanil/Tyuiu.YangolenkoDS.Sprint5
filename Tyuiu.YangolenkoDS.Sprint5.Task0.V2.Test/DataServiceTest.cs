b3bb0a04b1be19c74759ec0ace223380e288ca90
Tyuiu.YangolenkoDS@outlook.com <Tyuiu.YangolenkoDS@outlook.com>
2023-11-18 12:25:19 +0500

    Task0

--- Tyuiu.YangolenkoDS.Sprint5.Task0.V2.Test/DataServiceTest.cs
+++ Tyuiu.YangolenkoDS.Sprint5.Task0.V2.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.YangolenkoDS.Sprint5.Task0.V2.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\angol\source\repos\Tyuiu.YangolenkoDS.Sprint5\Tyuiu.YangolenkoDS.Sprint5.Task0.V2\bin\Debug.OutPutFileTask0.txt";
+            string path = @"C:\Users\angol\source\repos\Tyuiu.YangolenkoDS.Sprint5\Tyuiu.YangolenkoDS.Sprint5.Task0.V2\bin\Debug\OutPutFileTask0.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
