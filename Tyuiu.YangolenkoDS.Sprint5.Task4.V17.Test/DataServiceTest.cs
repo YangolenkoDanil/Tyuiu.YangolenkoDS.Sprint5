4d4647653aef4694a38bc1baadcfd681aa6f75d1
Tyuiu.YangolenkoDS@outlook.com <Tyuiu.YangolenkoDS@outlook.com>
2023-11-25 13:02:18 +0500

    Task4

---
+++ Tyuiu.YangolenkoDS.Sprint5.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.YangolenkoDS.Sprint5.Task4.V17.Lib;
+using System.IO;
+
+namespace Tyuiu.YangolenkoDS.Sprint5.Task4.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V17.txt";
+            FileInfo fileinfo = new FileInfo(path);
+
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.YangolenkoDS.Sprint5.Task4.V17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.YangolenkoDS.Sprint5.Task4.V17.Lib;
+
+namespace Tyuiu.YangolenkoDS.Sprint5.Task4.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Янголенко Д. С. | АСОиУб-23-1";
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                         *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                           *");
+            Console.WriteLine("* Задание #4                                                                        *");
+            Console.WriteLine("* Вариант #17                                                                       *");
+            Console.WriteLine("* Выполнил: Янголенко Данил Сергеевич | АСОиУб-23-1                                 *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле                       *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V17.txt";
+
+            Console.WriteLine("Данные находится в файле: " + path);
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
